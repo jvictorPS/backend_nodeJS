dc156e9163c964dfb43385b355618c7596096d3f
João Victor <j.victorbahia@gmail.com>
2023-11-09 06:05:13 -0300

    estudo dia 09/11

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -1,25 +1,16 @@
 const express = require('express')
+const checkListRouter = require('./src/routes/checklist')
 
 const app = express()
-
 app.use(express.json())
 
-const log = (req , res , next) => {
-    console.log(req.body)
-    console.log(`Data: ${Date.now()}`)
-    next()
-}
+app.use('/checklists', checkListRouter)
 
-app.use(log)
 
-app.get('/' , (req , res) => {
-    res.send('<h1>Minha lista de tarefas</h1>')
-})
-
-// PARA DEVOLVER RESPOSTAS EM FORMATO JSON
-app.get('/json' , (req , res) => {
-    res.json({title: 'lavar a louça', done: true})
-})
+// // PARA DEVOLVER RESPOSTAS EM FORMATO JSON
+// app.get('/json' , (req , res) => {
+//     res.json({title: 'lavar a louça', done: true})
+// })
 
 app.listen(3000 , () => {
     console.log('Servidor no ar')
